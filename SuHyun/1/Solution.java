a99e74829a1ed70e0e78684253200badeeca8c86
SyHyun Kim <37679352+khnemu11@users.noreply.github.com>
2024-01-08 16:31:00 +0900

    refact : 1번째 문제 불필요한 코드 제거

--- SuHyun/1/Solution.java
+++ SuHyun/1/Solution.java
@@ -1,11 +1,8 @@
-import java.util.*;
-
 class Solution {
     final int EXIST = 1;
     final int MODULAR = 10_007;
     
     public int solution(int n, int[] tops) {
-        int answer = 0;
         int[] dp = new int[n*2+2];
         dp[0]=1;        
         dp[1]=1;
@@ -17,6 +14,7 @@ class Solution {
                 dp[i] = (dp[i]%MODULAR + dp[i-1]%MODULAR)%MODULAR;
             }
         }
+        
         return dp[dp.length-1];
     }
 }
