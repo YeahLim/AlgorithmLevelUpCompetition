3b728255c2e0fe51d677a99e11a845ab6d0d47ae
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2024-01-09 12:27:00 +0900

    Update Main.java

--- YeahLim/1/Main.java
+++ YeahLim/1/Main.java
@@ -1,35 +1,41 @@
-class Solution {
-    
-    public int solution(int n, int[] money) {        
-        int[][] dp = new int[money.length + 1][n+1]; // [moeny종류][n원]       
-        
-        // dp[0][] = 0이다, 그 전 값을 사용하기 위해 money.length가 아닌, money.length+1사용
-        // 1번째 index부터 money 종류 시작
-        
-        for (int i = 1; i < money.length+1; i++) {
-            for (int j = 0; j < n+1; j++) {
-                
-                // dp 초기화 : 0원일 때
-                if (j == 0) {
-                    dp[i][j] = 1;
-                }
-                
-                else {
-                    
-                    // j - money[i-1] 음수일 때, 0 을 더한다
-                    if (money[i-1] > j) {
-                        dp[i][j] = dp[i-1][j];
-                    }   
-                    
-                    else {
-                        dp[i][j] = (dp[i-1][j] + dp[i][j - money[i-1]]) % 1000000007;
-                    }
-                }
-            }
-        }
-        
-        
-        
-        return dp[money.length][n];
-    }
+import java.util.*;
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		
+		// 수열 초기화
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] array = new int[n];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		// dp 초기화
+		int[] dp = new int[n];
+		dp[0] = 1;
+		
+		// 수열의 길이 구하기
+		for (int i = 1; i < array.length; i++) {
+			
+			int min = 0;
+			for (int j = 0; j < i; j++) {
+				if (array[i] > array[j]) {
+					min = Math.max(min,  dp[j]);
+				}
+			}
+			
+			dp[i] = min + 1;
+		}
+		
+        // 가장 긴 수열 구하기
+		int answer = 0;
+		for (int d : dp) {
+			answer = Math.max(answer,  d);
+		}
+		
+		System.out.println(answer);
+	}
 }
