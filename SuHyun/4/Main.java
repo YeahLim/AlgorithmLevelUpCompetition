1a54c92f70dc4dfa1d51f9af8ee602e250917d90
Kim SuHyunm <khnemu@naver.com>
2024-01-25 20:58:52 +0900

    docs : 2회차 4번째 문제 풀이
    240125 수현

---
+++ SuHyun/4/Main.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    final static int MOD = 10_007;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] upStairs = new int[N+1][10];
+        Arrays.fill(upStairs[1],1);
+
+        for(int i=2;i<=N;i++){
+            for(int j=0;j<10;j++){
+                for(int k=0;k<=j;k++){
+                    upStairs[i][j] = (upStairs[i][j] + upStairs[i-1][k]) % MOD;
+                }
+            }
+        }
+
+        System.out.println(Arrays.deepToString(upStairs));
+
+        int sum = 0;
+
+        for(int i=0;i<10;i++){
+            sum = (sum + upStairs[N][i]) % MOD;
+        }
+
+        System.out.println(sum);
+    }
+}
+
