829eb0327cedee467b882fd495bbc7a01cd4fcb2
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2024-01-25 17:21:07 +0900

    1_code

--- YeahLim/1/Main.java
+++ YeahLim/1/Main.java
@@ -2,41 +2,71 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+	
 	public static void main(String[] args) throws IOException {
 		
-		// 수열 초기화
+		// M 초기화
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] array = new int[n];
-		for (int i = 0; i < array.length; i++) {
-			array[i] = Integer.parseInt(st.nextToken());
-		}
+		int M = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 		
-		// dp 초기화
-		int[] dp = new int[n];
-		dp[0] = 1;
+		Set<Integer> set = new HashSet<>();
+		StringBuilder sb = new StringBuilder();
 		
-		// 수열의 길이 구하기
-		for (int i = 1; i < array.length; i++) {
+		while(M-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			String cmd = st.nextToken();
+			int num = 0;
 			
-			int min = 0;
-			for (int j = 0; j < i; j++) {
-				if (array[i] > array[j]) {
-					min = Math.max(min,  dp[j]);
+			
+			
+			if (cmd.equals("add")) {
+				num = Integer.parseInt(st.nextToken());
+				set.add(num);
+			}
+			
+			else if (cmd.equals("remove")) {
+				num = Integer.parseInt(st.nextToken());
+				set.remove(num);
+			}
+			
+			else if (cmd.equals("check")) {
+				num = Integer.parseInt(st.nextToken());
+				
+				if (set.contains(num)) {
+					sb.append(1);
+				}
+				else {
+					sb.append(0);
+				}
+				
+				sb.append("\n");
+			}
+			
+			else if (cmd.equals("toggle")) {
+				num = Integer.parseInt(st.nextToken());
+				
+				if (set.contains(num)) {
+					set.remove(num);
+				}
+				else {
+					set.add(num);
+				}
+			}
+
+			else if (cmd.equals("all")) {
+				for (int i = 1; i <= 20; i++) {
+					set.add(i);
 				}
 			}
 			
-			dp[i] = min + 1;
+			else if (cmd.equals("empty")) {
+				set.clear();
+			}
 		}
 		
-        // 가장 긴 수열 구하기
-		int answer = 0;
-		for (int d : dp) {
-			answer = Math.max(answer,  d);
-		}
+		System.out.println(sb);
 		
-		System.out.println(answer);
 	}
-}
 
+}
