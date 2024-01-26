c4dd855cdb9279cb091e699b4e18a598a7516943
wook <wook7416@naver.com>
2024-01-26 23:17:44 +0900

    jinwook

---
+++ Godseye/19/2022번.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+def get_c(x, y, width):
+  h1 = (x**2 - width**2)**0.5
+  h2 = (y**2 - width**2)**0.5
+  c = h1 * h2 / (h1 + h2)
+  return c
+
+def ans():
+  x, y, c = map(float, input().split())
+  start, end = 0, min(x,y)
+  res = 0
+  while (end - start) > 0.000001 :
+    width = (start + end) / 2
+    res = width
+    if get_c(x, y, width) >= c : # c를 구했는데 기존의 c보다 같거나 크면 w값을 키워주어서 h1, h2의 값을 작게 해주어야 한다.
+      start = width
+    else :
+      end = width  
+  print(res)
+
+ans()
---
+++ Godseye/19/README.md
@@ -0,0 +1,19 @@
+# [백준]  사다리 (2022번)
+
+## ⏰  **time**
+
+30분
+
+## :pushpin: **Algorithm**
+
+수학
+
+## ⏲️**Time Complexity**
+
+$O(N)$
+
+## :round_pushpin: **Logic**
+
+## :black_nib: **Review**
+
+- 
