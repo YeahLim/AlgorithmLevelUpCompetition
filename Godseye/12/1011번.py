5a347be118b4167115fa33262c7aa5951c8d6f16
wook <wook7416@naver.com>
2024-01-26 21:18:37 +0900

    jinwook

---
+++ Godseye/12/1011번.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    x, y = map(int, input().split())
+
+    distance = y - x
+    tmp = 0
+    cnt = 0
+    moving = 0
+
+    while tmp < distance:
+        cnt += 1
+        if cnt % 2 != 0:
+            moving += 1
+        tmp += moving
+    print(cnt)
---
+++ Godseye/12/README.md
@@ -0,0 +1,19 @@
+# [백준]  Fly me to the Alpha Centauri (1011번)
+
+## ⏰  **time**
+
+30분
+
+## :pushpin: **Algorithm**
+
+구현
+
+## ⏲️**Time Complexity**
+
+$O(N)$
+
+## :round_pushpin: **Logic**
+
+## :black_nib: **Review**
+
+- 
