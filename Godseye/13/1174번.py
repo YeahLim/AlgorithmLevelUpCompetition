62f5ea53d78382842a8dbba79234ec99233bc9df
wook <wook7416@naver.com>
2024-01-26 21:32:04 +0900

    jinwook

---
+++ Godseye/13/1174번.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = list()
+result = set()
+def dfs():
+    if len(arr) > 0:
+        result.add(int("".join(map(str, arr))))
+
+    for i in range(0, 10):
+        if len(arr) == 0 or arr[-1] > i:
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+try:
+    dfs()
+    result = list(result)
+    result.sort()
+    print(result[n - 1])
+except:
+    print(-1)
---
+++ Godseye/13/README.md
@@ -0,0 +1,19 @@
+# [백준]  줄어드는 수 (1174번)
+
+## ⏰  **time**
+
+30분
+
+## :pushpin: **Algorithm**
+
+DFS
+
+## ⏲️**Time Complexity**
+
+$O(N)$
+
+## :round_pushpin: **Logic**
+
+## :black_nib: **Review**
+
+- 
