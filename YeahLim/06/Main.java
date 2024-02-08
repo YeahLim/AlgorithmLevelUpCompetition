89cbc95c505afe0fb09ec04878101b246b24f2c9
Yeah'Lim <80014833+YeahLim@users.noreply.github.com>
2024-02-09 02:03:21 +0900

    06_2xN 타일링 2

--- YeahLim/06/Main.java
+++ YeahLim/06/Main.java
@@ -12,7 +12,10 @@ class Main {
 		// 2 * N 타일링 방법의 수 구하기
 		int[] dp = new int[N+1];
 		dp[1] = 1;
-		dp[2] = 3;
+		
+		if (N > 1) {
+			dp[2] = 3;
+		}
 				
 		for (int i = 3; i <= N; i++) {
 			dp[i] = (dp[i-1] + dp[i-2] * 2) % 10_007;
