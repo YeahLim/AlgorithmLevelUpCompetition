22e27065d7a0d65114f6e728e6a6390a780519e8
khnemu <khnemu@naver.com>
2024-02-19 18:06:16 +0900

    06_숫자 고르기

---
+++ SuHyun/06/Main.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] table = new int[N+1];
+
+        for(int i=1;i<=N;i++){
+            table[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        List<Integer> result = new ArrayList<>();
+
+        for(int start=1;start<=N;start++){
+            Queue<Integer> q = new LinkedList<>();
+            q.add(start);
+
+            boolean[] visited = new boolean[N+1];
+
+            while(!q.isEmpty()){
+                int curr = q.poll();
+
+                if(visited[curr]){
+                    if(curr == start){
+                        result.add(curr);
+                        count++;
+                    }
+                    break;
+                }
+
+                visited[curr] = true;
+                q.add(table[curr]);
+            }
+        }
+
+        System.out.println(count);
+
+        for(int num : result){
+            System.out.println(num);
+        }
+    }
+}
