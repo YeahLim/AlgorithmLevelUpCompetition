b8fc0c0d2c8897dbeff4269f39fc4ad45447aff7
{Ryul} <{kjr5876@gmail.com}>
2024-02-22 21:05:57 +0900

    10_가장 큰 증가하는 부분 수열

---
+++ Jongryul/10/11055.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [i for i in arr]
+for i in range(n):
+	for j in range(i):
+		if arr[j] < arr[i]:
+			dp[i] = max(dp[j]+arr[i], dp[i])
+print(max(dp))
