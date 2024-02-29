4842fd5062a7aff1a67067ee174622dd3272224c
wook <wook7416@naver.com>
2024-02-29 21:52:35 +0900

    01_ccw

---
+++ Godseye93/01/11758번.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+p1x, p1y = map(int,input().split())
+p2x, p2y = map(int,input().split())
+p3x, p3y = map(int,input().split())
+
+result = p1x*p2y + p2x*p3y + p3x*p1y - p2x*p1y - p3x*p2y - p1x*p3y
+
+if result > 0:
+    print(1)
+elif result < 0:
+    print(-1)
+else:
+    print(0)
---
+++ Godseye93/01/README.md
@@ -0,0 +1,19 @@
+# [백준]  CCW(11758번)
+
+## ⏰  **time**
+
+30분
+
+## :pushpin: **Algorithm**
+
+구현
+
+## ⏲️**Time Complexity**
+
+$O(N)$
+
+## :round_pushpin: **Logic**
+
+- ## :black_nib: **Review**
+
+- 
