ab7de0a742a649b2ee601308e88f1b62a04b0b73
wook <wook7416@naver.com>
2024-03-08 22:00:15 +0900

    08_스네이크 버드

---
+++ Godseye93/08/16435번.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n,l = map(int,input().split())
+
+data = list(map(int,input().split()))
+
+data.sort()
+
+for i in data:
+    if i <= l:
+        l += 1
+    else:
+        break
+print(l)
---
+++ Godseye93/08/README.md
@@ -0,0 +1,19 @@
+# [백준]  스네이크버드 (16435번)
+
+## ⏰  **time**
+
+30분
+
+## :pushpin: **Algorithm**
+
+정렬
+
+## ⏲️**Time Complexity**
+
+$O(N)$
+
+## :round_pushpin: **Logic**
+
+- ## :black_nib: **Review**
+
+- 
