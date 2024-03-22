2890ee67963c0dafc5b0f4d3f7397e15cfa6ac5f
Yoojong <93518751+YoojongChoi@users.noreply.github.com>
2024-03-22 18:29:55 +0900

    05_요세푸스

---
+++ Yoojong/05/11866.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+n, k = map(int, sys.stdin.readline().split())
+
+deq = deque([i for i in range(1, n+1)])
+
+ans = []
+while len(deq) != 0:
+    for _ in range(k-1):
+        deq.append(deq.popleft())
+    ans.append(str(deq.popleft()))
+
+print('<' + ', '.join(ans)+ '>')
