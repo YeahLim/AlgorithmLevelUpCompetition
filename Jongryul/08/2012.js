2d98860bde4835e053cde758d80923c20b4957a3
{Ryul} <{kjr5876@gmail.com}>
2024-03-29 21:17:20 +0900

    08등수 매기기

---
+++ Jongryul/08/2012.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const n = Number(input[0]);
+const arr = input.slice(1, n + 1).map(Number);
+
+arr.sort((a, b) => a - b);
+let cnt = 0;
+for (let i = 0; i < n; i++) {
+  cnt += Math.abs(arr[i] - (i + 1));
+}
+console.log(cnt);
---
+++ Jongryul/08/2012.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+arr = sorted([int(input()) for _ in range(n)])
+cnt = 0
+for i in range(n):
+    cnt += abs(arr[i] - (i+1))
+print(cnt)
