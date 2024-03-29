68cd31e21881e6769f1e1969fb42aa263299bc92
{Ryul} <{kjr5876@gmail.com}>
2024-03-29 21:25:05 +0900

    08_등수 매기기

--- Jongryul/08/2012.js
+++ Jongryul/08/2012.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
-
 const n = Number(input[0]);
 const arr = input.slice(1, n + 1).map(Number);
 
--- Jongryul/08/2012.py
+++ Jongryul/08/2012.py
@@ -2,6 +2,7 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 
+
 arr = sorted([int(input()) for _ in range(n)])
 cnt = 0
 for i in range(n):
