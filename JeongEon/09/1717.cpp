3ffb05129a74190711b391b31cd3cb4495cabb29
JeongEon <113885866+JeongEon8@users.noreply.github.com>
2024-04-05 21:36:52 +0900

    09_집합의 표현

---
+++ JeongEon/09/1717.cpp
@@ -0,0 +1,53 @@
+// 코딩 테스트 스터디 11회차
+// 난이도: 골드 5
+// 문제번호: 1717번
+// 문제이름: 집합의 표현
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int n, m;
+int parent[1000001];
+
+int getParent(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = getParent(parent[x]);
+}
+void unionParent(int a, int b) {
+    a = getParent(a);
+    b = getParent(b);
+    if (a > b) parent[a] = b;
+    else parent[b] = a;
+}
+void findParent(int a, int b) {
+    a = getParent(a);
+    b = getParent(b);
+    if (a == b) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        parent[i] = i;
+    }
+    for (int i = 0; i < m; i++) {
+        int o, a, b;
+        cin >> o >> a >> b;
+        if (!o) {// union연산
+            unionParent(a, b);
+        }
+        else { //find연산
+            findParent(a, b);
+        }
+
+    }
+
+    return 0;
+}
