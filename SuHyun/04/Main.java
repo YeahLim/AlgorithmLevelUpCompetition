9a08e7f49f72fae186ec9ca7fb81e42007548b48
khnemu <khnemu@naver.com>
2024-04-09 19:34:29 +0900

    05_자두나무

--- SuHyun/04/Main.java
+++ SuHyun/04/Main.java
@@ -1,45 +1,40 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
 
+/*
+ * 
+ * 걸린시간 : 26분
+ * 
+ * 현재 값을 삭제한 수열의 최대합 = max(이전 삭제하지 않은 최대합에서 현재 값 삭제하기, 이전 삭제한 최대합에서 현재값 더하기)
+ * 현재 값을 삭제하지 않은 수열의 최대합= = max(이전 삭제하지 않는 최대합 + 현재 값, 현재 값)
+ * */
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        long[] num = new long[N+1];
-        long[] sum = new long[N+1];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for(int i=1;i<=N;i++){
-            num[i] = Long.parseLong(st.nextToken());
-            sum[i] = sum[i-1] + num[i];
-        }
-
-        long[][] dp = new long[N+1][4];
-        long count = 0;
-
-        if(sum[N] %4 ==0){
-            long target = sum[N]/4;
-
-            dp[0][0] = 1;   //dp[1][1]이 가능한 경우을 위한 값
-
-            for(int i=1;i<=N;i++){
-                dp[i][0] = 1;   //dp[n][1]이 가능한 경우를 위한 값
-                for(int j=1;j<=3;j++){
-                    dp[i][j] = dp[i-1][j];  //이전까지 가능했던 경우의 수를 가져옴
-
-                    if(sum[i] == target*j){ //현재까지의 누적합으로 부분 수열의 합이 가능하면
-                        //이전 크기의 부분 수열이 가능한 만큼 추가로 되는 경우의 수이다.
-                        dp[i][j] += dp[i-1][j-1];
-                    }
-                }
-            }
-
-            count = dp[N-1][3];
-        }
-
-        System.out.println(count);
-    }
+	static int NOT_DELETE = 0;
+	static int DELETE = 1;
+	
+	public static void main(String[] args) throws Exception {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       int n = Integer.parseInt(br.readLine());
+       
+       int[]num = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+       int[][]dp = new int[2][n];
+       
+       dp[NOT_DELETE][0] = num[0];
+       dp[DELETE][0] = num[0];
+       int max = num[0];
+       
+       //DP 진행
+       for(int i=1;i<dp[0].length;i++) {
+    	   dp[NOT_DELETE][i] = Math.max(dp[NOT_DELETE][i-1] + num[i], num[i]);
+    	   dp[DELETE][i] = Math.max(dp[NOT_DELETE][i-1], dp[DELETE][i-1] + num[i]);
+    	   
+    	   max = Math.max(dp[DELETE][i],max);
+    	   max = Math.max(max,dp[NOT_DELETE][i]);
+       }
+       
+       System.out.println(max);
+       
+	}
 }
