4e771de251dab2676b2ec682d99a2711cfa7c67d
Kim SuHyunm <khnemu@naver.com>
2024-04-10 22:33:47 +0900

    08_

---
+++ SuHyun/08/Main.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+/*
+ *
+ * 풀이 시간 : 46분
+ *
+ * */
+
+public class Main {
+    static int USED = 0;
+    static int UNUSED = 1;
+    static List<Integer> graph[];
+    //최대 주민 합[현재 마을을 사용했는지/안했는지][마을 번호]
+    static int[][] dp;
+    static int[] people;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        people = new int[N+1];
+        graph = new ArrayList[N+1];
+        dp = new int[2][N+1];
+        visited = new boolean[N+1];
+
+        for(int i=1;i<=N;i++){
+            graph[i] = new ArrayList<>();
+            people[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=1;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+
+            graph[start].add(end);
+            graph[end].add(start);
+        }
+
+        System.out.println(Math.max(getMaxPeople(USED,1), getMaxPeople(UNUSED,1)));
+    }
+
+    //해당 노드를 사용했을 때/사용하지 않았을 때의 최대 주민 합을 알려주는 메소드
+    public static int getMaxPeople(int status,int n){
+        if(visited[n]){
+            return dp[status][n];
+        }
+
+        visited[n] = true;
+        dp[USED][n] = people[n];
+
+        for(int next : graph[n]){
+            //부모에서 자식으로 내려오고 있으므로 이미 방문했다면 부모 노드이므로 계산하지 않는다.
+            if(visited[next]){
+                continue;
+            }
+            int unUsedSum = getMaxPeople(UNUSED,next);;
+            int usedSum = getMaxPeople(USED,next);;
+
+            dp[USED][n] += unUsedSum;
+            dp[UNUSED][n] += Math.max(unUsedSum,usedSum);
+        }
+
+        return dp[status][n];
+    }
+}
