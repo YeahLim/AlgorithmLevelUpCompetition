158c02339ae5ec315cd6654814d13cad7393e1bf
YeahLim <yeahleem2@gmail.com>
2024-05-13 21:35:11 +0900

    01_리모콘

---
+++ YeahLim/01/Main.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	
+	static int N;
+	static boolean[] brokenButton;
+	static int answer, temp;
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		// 변수 초기화
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		// 고장난 리모콘이 없을 경우
+		if (M == 0) {
+			answer = Math.min(Math.abs(N - 100), Integer.toString(N).length());
+			System.out.println(answer);
+			return;
+		}
+		
+		brokenButton = new boolean[10];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		while (M-- > 0) {
+			brokenButton[Integer.parseInt(st.nextToken())] = true;
+		}
+		
+		
+		
+		// +1, -1로만 채널 탐색
+		answer = Math.abs(N - 100);
+		
+		// 숫자버튼 돌리면서 채널 탐색
+		for (int channel = 0; channel < 1000000; channel++) {
+	
+			// 고장난 버튼이 포함돼있는지 확인
+			boolean broken = false;
+			String strChannel = Integer.toString(channel);
+			for (int i = 0; i < strChannel.length(); i++) {
+				if (brokenButton[strChannel.charAt(i) - '0']) {
+					broken = true;
+					break;
+				}
+			}
+			
+			// 고장난 버튼이 포함 안돼있다면
+			if (!broken) {
+				answer = Math.min(answer, strChannel.length() + Math.abs(channel - N));
+			}	
+		}
+		
+		
+		System.out.println(answer);
+	}
+
+
+}
---
+++ YeahLim/01/README.md
@@ -0,0 +1,18 @@
+# [백준] 리모콘 (1107)
+ 
+## ⏰  **time**
+시간초과
+
+## :pushpin: **Algorithm**
+완탐
+
+## ⏲️**Time Complexity**
+
+$O(N)$
+
+## :round_pushpin: **Logic**
+그냥 for문으로 처음부터 끝까지 돌리기
+
+## :black_nib: **Review**
+- 처음에 예외처리를 잘못해서 오래걸렸다.
+- 잠을 못잤더니 머리가 안돌아간다... 으으... 다음부턴 멀정할때 후딱 풀어야겠다https://help.acmicpc.net/judge/rte
