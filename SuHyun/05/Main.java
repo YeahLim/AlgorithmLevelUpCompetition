59ceb3689fcfbb9bdd39e23e3def7f407c238e18
Kim SuHyunm <khnemu@naver.com>
2024-07-02 02:55:51 +0900

    05_∑|ΔEasyMAX|

---
+++ SuHyun/05/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        int[]a = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for(int q=0;q<Q;q++){
+            st = new StringTokenizer(br.readLine());
+
+            int i = Integer.parseInt(st.nextToken())-1;
+            int j = Integer.parseInt(st.nextToken())-1;
+
+            //j-1<1 인경우는 0 출력
+            if(j-1< i){
+                System.out.println(0);
+            }
+            //아닌경우는 누적합 실행
+            else{
+                int sum = 0;
+
+                for(int r=i+1;r<=j;r++){
+                    sum+=Math.abs(a[r]-a[r-1]);
+                }
+
+                System.out.println(sum);
+            }
+        }
+    }
+}
---
+++ SuHyun/05/README.md
@@ -0,0 +1,24 @@
+# [백준 - 실버 4] ∑|ΔEasyMAX| (17203)
+ 
+## ⏰  **time**
+
+10분
+
+## :pushpin: **Algorithm**
+
+구현
+
+## ⏲️**Time Complexity**
+
+$O(N)$
+
+## :round_pushpin: **Logic**
+
+1. 문제풀이
+
+주어진 조건대로 구현하면 된다.
+
+이때 j-1<i인 경우 모든 범위가 0이 되므로 0을 그대로 출력하면 된다.
+
+## :black_nib: **Review**
+- 조건을 상세히 따져 예외 조건을 생각하자
