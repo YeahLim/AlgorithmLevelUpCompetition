8fd20175b835e39fc09dec97c95f5e3b8666e27a
Kim SuHyunm <khnemu@naver.com>
2024-07-02 02:02:14 +0900

    03_전생했더니 슬라임 연구자였던 건에 대하여 (Easy)

---
+++ SuHyun/03/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        boolean valid = true;
+        int count = 1;  //소인수 분해 횟수
+
+        //소인수 분해 횟수 구하기
+        while(valid){
+            valid = false;
+
+            for(int i=2;i<=Math.sqrt(N);i++){
+                //소인수 분해를 할 수 있다면 인수 분해 진행
+                if(N % i == 0){
+                    count++;
+                    N/=i;
+                    valid= true;
+                    break;
+                }
+            }
+        }
+
+        //완전 이진트리의 높이 구하기
+        int height = (int)Math.ceil(Math.log(count)/Math.log(2));
+
+        System.out.println(height);
+    }
+}
