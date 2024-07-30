0fdbb2a87a279d1dbd4a8f63d2f6f83cdc445189
yeahLim <yeahleem2@gmail.com>
2024-07-30 23:12:06 +0900

    02_공유기 설치

---
+++ YeahLim/02/Main.java
@@ -0,0 +1,59 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+	static int[] house;
+	static int answer = 1;
+	
+	public static void main(String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int C = sc.nextInt();
+		
+		house = new int[N];
+		for (int i = 0; i < N; i++) {
+			house[i] = Integer.parseInt(sc.next());
+		}
+		Arrays.sort(house);
+		
+		int start = 1; // 최소 거리가 1이므로
+		int end = house[N-1] - house[0]; // house[N-1]보다 효율적
+		
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			
+			if (checkInstall(mid) >= C){
+				start = mid + 1;
+				answer = mid;
+			}
+			else {
+				end = mid - 1;
+			}
+		}
+		
+		System.out.println(answer);
+	}
+
+	private static int checkInstall(int dist) {
+		
+		int count = 1;
+		int lastInstalledHouse = house[0];
+		
+		for (int i = 1; i < house.length; i++) {
+			if (house[i] - lastInstalledHouse >= dist) {
+				count++;
+				lastInstalledHouse = house[i];
+			}
+			// 모든 집에 반드시 설치되어야하는 것이 아니므로
+//			else {
+//				break;
+//			}
+		}
+		
+		return count;
+	}
+
+}
