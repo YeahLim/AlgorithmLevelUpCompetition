5f2be9b4021b6e911052dbb0c2d4033f50c4a443
{Ryul} <{kjr5876@gmail.com}>
2024-09-11 20:34:22 +0900

    02_안

---
+++ Jongryul/02/1535.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const n = Number(input[0]);
+const hp = [0, ...input[1].split(" ").map(Number)];
+const happy = [0, ...input[2].split(" ").map(Number)];
+const dp = Array.from({ length: n + 1 }, () =>
+  Array.from({ length: 101 }, () => 0)
+);
+
+for (let i = 1; i < n + 1; i++) {
+  for (let j = 1; j < 101; j++) {
+    if (hp[i] <= j) {
+      dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - hp[i]] + happy[i]);
+    } else {
+      dp[i][j] = dp[i - 1][j];
+    }
+  }
+}
+
+console.log(dp[n][99]);
---
+++ Jongryul/02/README.md
@@ -0,0 +1,15 @@
+# [백준 - 실버 2 ] 안녕 (1535번)
+
+https://www.acmicpc.net/problem/1535
+
+## ⏰ **time**
+
+1시간
+
+## :pushpin: **Algorithm**
+
+dp
+
+## :round_pushpin: **Logic**
+
+1. 냅색문제이다.
