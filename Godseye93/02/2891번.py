8afd46c604a0d7db00e72f20aa0229125a7749b8
wook <wook7416@naver.com>
2024-10-17 22:07:06 +0900

    02_카악과 강풍

---
+++ Godseye93/02/2891번.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n, s, r = map(int, input().split())
+broke = list(map(int, input().split()))
+redun = list(map(int, input().split()))
+
+broke, redun = list(set(broke) - set(redun)), list(set(redun) - set(broke))
+
+ans = 0
+for i in broke:
+    if i - 1 in redun:
+        redun.remove(i - 1)
+    elif i + 1 in redun:
+        redun.remove(i + 1)
+    else:
+        ans += 1
+
+print(ans)
---
+++ Godseye93/02/README.md
@@ -0,0 +1,21 @@
+# [백준]  카악과 강풍(2891번)
+
+## ⏰  **time**
+
+30분
+
+## :pushpin: **Algorithm**
+
+구현
+
+## ⏲️**Time Complexity**
+
+$O(N)$
+
+## :round_pushpin: **Logic**
+
+## :black_nib: **Review**
+
+```
+
+```
