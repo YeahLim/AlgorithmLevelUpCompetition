5a2fb81054e53a6b96f063191cc69fe608fa5108
wook <wook7416@naver.com>
2024-12-26 21:17:53 +0900

    02_파스칼의삼각형

---
+++ Godseye93/02/16395번.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+ans = [[] for _ in range(n)]
+
+for i in range(0, n):
+    for j in range(0, i+1):
+        if i == 0 or j == 0 or j == i:
+            ans[i].append(1)
+        else:
+            ans[i].append(ans[i-1][j-1] + ans[i-1][j])
+
+print(ans[n-1][k-1])
---
+++ Godseye93/02/README.md
@@ -0,0 +1,21 @@
+# [백준]  파스칼의 삼각형(16395번)
+
+## ⏰  **time**
+
+30분
+
+## :pushpin: **Algorithm**
+
+구현
+
+## ⏲️**Time Complexity**
+
+$O(N)$
+
+## :round_pushpin: **Logic**
+
+## :black_nib: **Review**
+
+```
+
+```
