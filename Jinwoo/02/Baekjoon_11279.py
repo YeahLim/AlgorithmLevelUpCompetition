7d540b5d80170083e6a3f03ca755e69d40ecafc4
rttitity <jinwoo25803@gmail.com>
2025-02-20 17:40:40 +0900

    02_최대 힙

---
+++ Jinwoo/02/Baekjoon_11279.py
@@ -0,0 +1,21 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+N = int(input())        # 연산의 개수
+heap = []
+
+for i in range(N):
+    x = int(input())    # 연산에 대한 정보 입력
+
+    # 0이 아닐 경우 힙에 삽입
+    if x != 0:
+        heapq.heappush(heap, -x)
+
+    # 0일 경우 최대 힙 출력
+    else:
+        # 출력 후 배열에서 제거
+        try:
+            print(-heapq.heappop(heap))
+        except:
+            print(0)
