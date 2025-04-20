9f49e593a6135ae963acb886bee662fe17e70d12
yeahLim <yeahleem2@gmail.com>
2025-04-20 20:44:36 +0900

    08_여행경로

---
+++ YeahLim/08/Solution.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<string>> tickets;
+vector<bool> visited;
+bool isSearched; // 가지치기용 (백트래킹)
+vector<string> answer;
+
+void searchRoute(string airport, int depth, vector<string>& route) {
+   
+    if (isSearched) {
+        return;
+    }
+   
+    if (depth == tickets.size()) {
+        isSearched = true;
+        answer = route;
+        return;
+    }
+   
+    for (int i = 0; i < tickets.size(); i++) {
+        
+        if (!visited[i] && airport == tickets[i][0]) {   
+            visited[i] = true;
+            route.push_back(tickets[i][1]);
+            searchRoute(tickets[i][1], depth+1, route);
+            route.pop_back();
+            visited[i] = false;
+        }
+    }
+}
+
+vector<string> solution(vector<vector<string>> tickets) {
+
+    visited = vector<bool>(tickets.size(), false);
+    isSearched = false;
+   
+    // 1. tickets 알파벳 정렬
+    sort(tickets.begin(), tickets.end());
+    ::tickets = tickets;
+
+    // 2. dfs로 루트 구하기
+    vector<string> route = {"ICN"};
+    searchRoute("ICN", 0, route);
+   
+    return answer;
+}
