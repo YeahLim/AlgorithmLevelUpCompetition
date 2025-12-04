eeb116bf2c4ef7d891a51727485b6b92d099bfc6
wook <wook7416@naver.com>
2025-12-04 18:38:55 +0900

    02_연속부분최대곱

---
+++ Godseye93/02/2670번.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [float(input()) for _ in range(N)]
+
+dp = arr[:]
+for i in range(1, N):
+    dp[i] = max(dp[i], dp[i-1] * arr[i])
+
+print(f"{max(dp):.3f}")
