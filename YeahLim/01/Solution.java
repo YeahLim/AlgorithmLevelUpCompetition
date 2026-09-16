f32b66e7ddc5744d30b3c4864d7f308ea1eec855
yelim_go <yelim_go@tmaxsoft.com>
2026-09-16 17:09:05 +0900

    01_최대공약수와 최소공배수

---
+++ YeahLim/01/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int gcd = gcd(n, m);
+        return new int[] {gcd, n * m / gcd};
+    }
+    
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
